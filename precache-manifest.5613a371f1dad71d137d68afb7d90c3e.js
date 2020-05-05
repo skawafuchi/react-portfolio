3a430a3b68fc7c55db56efd40b3b7d49abeb723b
Steven Kawafuchi <skawafuchi@gmail.com>
2020-05-05 16:25:55 -0700

    Updates

---
+++ precache-manifest.5613a371f1dad71d137d68afb7d90c3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3faf76f58313b956414b930f6523e858",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "85a753778bcb8779e80d",
+    "url": "/react-portfolio/static/js/2.c8653838.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.c8653838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a258fcb8d871879b5875",
+    "url": "/react-portfolio/static/js/main.66615181.chunk.js"
+  },
+  {
+    "revision": "4f2fc26c165b9cc33ce0",
+    "url": "/react-portfolio/static/js/runtime-main.63d2ede2.js"
+  }
+]);
